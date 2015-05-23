1eb55b1adee4c846f3eec03d39603959441f4f2b
Kaneraz <kaneraz@hotmail.com>
2015-05-23 16:11:03 -0500

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,92 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name turnTimerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the turnTimerApp
+ */
+angular.module('turnTimerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.players = [];
+
+    $scope.selected = 0;
+
+    var moveUp = function() {
+      move(-1);
+      $scope.$apply();
+    };
+    var moveDown = function() {
+      move(1);
+      $scope.$apply();
+    };
+		var swapUp = function() {swap(-1);};
+		var swapDown = function() {swap(1);};
+
+    $scope.bloody = function(index) {
+	    $scope.players[index].bloodied = !$scope.players[index].bloodied;
+    };
+
+    $scope.remove = function(index) {
+      $scope.players.splice(index, 1);
+      if($scope.selected > $scope.players.length - 1) {
+        $scope.selected = $scope.players.length - 1;
+      }
+    };
+
+		var events = [
+      { event: 'keydown:40', action: moveDown, description: 'Down arrow' },
+      { event: 'keydown:38', action: moveUp, description: 'Up arrow' },
+			{ event: 'keydown:37', action: swapUp, description: 'Left arrow' },
+			{ event: 'keydown:39', action: swapDown, description: 'Right arrow' },
+		];
+
+    $scope.add = function() {
+      var name = { name: $scope.addName, bloodied: false };
+      $scope.players.splice($scope.selected + 1, 0, name);
+      $scope.addName = '';
+      move(1);
+    }; 
+
+		events.forEach(function(currentValue) { 
+			$scope.$on(currentValue.event, currentValue.action);
+		});
+
+		var swap = function(direction) {
+			if(direction < 0 && $scope.selected === 0) {
+				return;
+			}
+			if(direction > 0 && $scope.selected === $scope.players.length -1) {
+				return;
+			}
+			var temp = $scope.players[$scope.selected];
+			$scope.players[$scope.selected] = $scope.players[$scope.selected + direction];
+			$scope.players[$scope.selected + direction] = temp;
+			move(direction);
+      $scope.$apply();
+		};
+
+    var move = function(direction) {
+      $scope.selected = ($scope.selected + direction) % $scope.players.length;
+      if($scope.selected < 0)
+      {
+				$scope.selected += $scope.players.length;
+      }
+    };
+  })
+	.directive('keypressEvents', ['$document', '$rootScope', function($document, $rootScope) {
+		return {
+			restrict: 'A',
+			link: function() {
+        $document.bind('keydown', function(e) {
+          $rootScope.$broadcast('keydown', e);
+          $rootScope.$broadcast('keydown:' + e.which, e);
+        });
+        $document.bind('keypress', function(e) {
+          $rootScope.$broadcast('keypress', e);
+          $rootScope.$broadcast('keypress:' + e.which, e);
+        });
+      }
+    };
+}]);
